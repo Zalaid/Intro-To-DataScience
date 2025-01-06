cc7f5b0cb5b29566b264cadb67c378bb8c2cd084
Zalaid <zalaidbutt1@gmail.com>
2025-01-07 00:51:02 +0500

    script

--- random_commits.py
+++ random_commits.py
@@ -12,30 +12,34 @@ def get_random_date_in_last_month():
     )
     return random_date.strftime("%Y-%m-%d %H:%M:%S")
 
-# Number of commits to make on each random day
-days_to_commit = random.randint(3, 7)  # Random days for commits
+# Number of commits to make on each random day (between 3 and 7)
+commits_per_day_range = (3, 7)
 
 # Your GitHub email
 github_email = "zalaidbutt1@gmail.com"  # Replace with your GitHub email
 
 # Create a set of random commit dates for the previous month
-commit_dates = sorted([get_random_date_in_last_month() for _ in range(days_to_commit)])
+commit_dates = [get_random_date_in_last_month() for _ in range(random.randint(1, 6))]
 
 for commit_date in commit_dates:
-    # Write something random in the file for the commit
-    with open("commit_file.txt", "a") as file:
-        file.write(f"Commit made on {commit_date}\n")
-    
-    # Stage the file
-    os.system("git add commit_file.txt")
-    
-    # Set the GIT_AUTHOR_DATE and GIT_COMMITTER_DATE environment variables
-    os.environ["GIT_AUTHOR_DATE"] = commit_date
-    os.environ["GIT_COMMITTER_DATE"] = commit_date
-    os.environ["GIT_AUTHOR_EMAIL"] = github_email  # Set author email
-    os.environ["GIT_COMMITTER_EMAIL"] = github_email  # Set committer email
-    
-    # Make the commit
-    os.system(f'git commit -m "Commit made on {commit_date}"')
+    # Determine how many commits to make on this date (between 3 and 7)
+    num_commits = random.randint(commits_per_day_range[0], commits_per_day_range[1])
+
+    for _ in range(num_commits):
+        # Write something random in the file for each commit
+        with open("commit_file.txt", "a") as file:
+            file.write(f"Commit made on {commit_date}\n")
+        
+        # Stage the file
+        os.system("git add commit_file.txt")
+        
+        # Set the GIT_AUTHOR_DATE and GIT_COMMITTER_DATE environment variables
+        os.environ["GIT_AUTHOR_DATE"] = commit_date
+        os.environ["GIT_COMMITTER_DATE"] = commit_date
+        os.environ["GIT_AUTHOR_EMAIL"] = github_email  # Set author email
+        os.environ["GIT_COMMITTER_EMAIL"] = github_email  # Set committer email
+        
+        # Make the commit
+        os.system(f'git commit -m "Commit made on {commit_date}"')
 
 print("Commits completed for previous month.")
